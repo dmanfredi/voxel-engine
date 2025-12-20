c1f68b2a2a01d8aa45a794cb4302b31aacd0abc6
dmanfredi <dylan.k.manfredi@gmail.com>
2025-12-20 15:10:38 -0500

    seperate out shader

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { mat4, vec3 } from 'wgpu-matrix';
+import MainShader from './shader';
 
 // TODO OF SORTS?
 // instancing?
@@ -239,34 +240,7 @@ async function main(): Promise<void> {
 	});
 
 	const module = device.createShaderModule({
-		code: /*wgsl*/ `
-			struct Uniforms {
-				matrix: mat4x4f,
-			}
-
-			struct Vertex {
-				@location(0) position: vec4f,
-				@location(1) color: vec4f,
-			}
-
-			struct VSOutput {
-				@builtin(position) position: vec4f,
-				@location(0) color: vec4f,
-			}
-
-			@group(0) @binding(0) var<uniform> uni: Uniforms;
-
-			@vertex fn vs(vert: Vertex) -> VSOutput {
-				var vsOut: VSOutput;
-  				vsOut.position = uni.matrix * vert.position;
-				vsOut.color = vert.color;
-				return vsOut;
-			}
-
-			@fragment fn fs(vsOut: VSOutput) -> @location(0) vec4f {
-				return vsOut.color;
-			}
-		`,
+		code: MainShader,
 	});
 
 	const pipeline = device.createRenderPipeline({
